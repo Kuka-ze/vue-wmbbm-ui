030a2b2fea70d78d6f8d724f798c61f4958f0eff
shuaiZe <vuejs1024@163.com>
2021-07-20 14:32:02 +0800

    配置ui库

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>111111111111</div>
+</template>
+
+<script>
+export default {
+  name: "dashboard",
+  setup() {
+    return {
+      data,
+      options,
+      options2,
+      todoList,
+      role,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
